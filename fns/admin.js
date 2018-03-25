ddccf0ba8c9bf66877e94863439a84a2af16f11d
Brad Cornes <bradlc41@gmail.com>
2018-03-25 15:30:34 +0100

    iframe

--- fns/admin.js
+++ fns/admin.js
@@ -1,9 +1,32 @@
 exports.handler = function(event, context, callback) {
+  const html = `
+    <!doctype html>
+    <html lang="en">
+      <head>
+        <meta charset="utf-8">
+        <title>Hello, world</title>
+        <style>
+        html, body {
+          height: 100%;
+        }
+        iframe {
+          width: 100%;
+          height: 100%;
+          border: 0;
+        }
+        </style>
+      </head>
+      <body>
+        <iframe src="${process.env.URL}"></iframe>
+      </body>
+    </html>
+  `
+
   callback(null, {
     statusCode: 200,
     headers: {
-      'Content-Type': 'application/json',
+      'Content-Type': 'text/html',
     },
-    body: JSON.stringify(process.env),
+    body: html,
   })
 }
