fbbeb84be033daac2f5c60a297e6bb930fb77c2a
Brad Cornes <bradlc41@gmail.com>
2018-03-25 18:19:13 +0100

    identity debug

--- src/fns/debug.js
+++ src/fns/debug.js
@@ -1,9 +1,11 @@
 exports.handler = function(event, context, callback) {
+  const { identity, user } = context.clientContext
+
   callback(null, {
     statusCode: 200,
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(process.env),
+    body: JSON.stringify({ identity, user }),
   })
 }
