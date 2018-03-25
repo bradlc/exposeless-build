5db6e8a8813ea922a42eb7ac849a4c61cc7270be
Brad Cornes <bradlc41@gmail.com>
2018-03-25 19:47:00 +0100

    hoist sequelize stuff

--- src/fns/update.js
+++ src/fns/update.js
@@ -1,32 +1,32 @@
 require('dotenv').config()
 const Sequelize = require('sequelize')
 
-exports.handler = function(event, context, callback) {
-  const sequelize = new Sequelize(
-    process.env.MYSQL_DATABASE,
-    process.env.MYSQL_USER,
-    process.env.MYSQL_PASSWORD,
-    {
-      dialect: 'mysql',
-      host: process.env.MYSQL_HOST,
-      port: 3306,
-    }
-  )
+const sequelize = new Sequelize(
+  process.env.MYSQL_DATABASE,
+  process.env.MYSQL_USER,
+  process.env.MYSQL_PASSWORD,
+  {
+    dialect: 'mysql',
+    host: process.env.MYSQL_HOST,
+    port: 3306,
+  }
+)
 
-  const Editable = sequelize.define(
-    'netlifyeditable',
-    {
-      path: {
-        type: Sequelize.STRING,
-        unique: true,
-      },
-      value: {
-        type: Sequelize.TEXT,
-      },
+const Editable = sequelize.define(
+  'netlifyeditable',
+  {
+    path: {
+      type: Sequelize.STRING,
+      unique: true,
     },
-    { underscored: true }
-  )
+    value: {
+      type: Sequelize.TEXT,
+    },
+  },
+  { underscored: true }
+)
 
+exports.handler = function(event, context, callback) {
   const body = JSON.parse(event.body)
 
   Editable.sync().then(() => {
