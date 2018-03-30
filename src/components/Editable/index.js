61222645faf2ff85d58376383823caf58cec9b2f
Brad Cornes <bradlc41@gmail.com>
2018-03-30 18:00:56 +0100

    draftjs???

--- src/components/Editable/index.js
+++ src/components/Editable/index.js
@@ -4,10 +4,12 @@ import React, { Component } from 'react'
 class Text extends Component {
   constructor(props) {
     super(props)
+    this.state = {}
+    this.onChange = editorState => this.setState({ editorState })
   }
 
-  render() {
-    const value = this.context.editables.filter(x => {
+  componentDidMount() {
+    let value = this.context.editables.filter(x => {
       return (
         x.node.page === this.context.pageName && x.node.name === this.props.name
       )
@@ -20,34 +22,80 @@ class Text extends Component {
         x => x.page === this.context.pageName && x.name === this.props.name
       )[0]
 
-    return (
-      <div
-        contentEditable
-        onBlur={e => {
-          // window.fetch('/.netlify/functions/update', {
-          //   method: 'post',
-          //   headers: {
-          //     'Content-Type': 'application/json',
-          //   },
-          //   body: JSON.stringify({
-          //     path: `${this.context.pageName}!${this.props.name}`,
-          //     value: e.target.textContent,
-          //   }),
-          // })
-          window.parent &&
-            window.parent.save({
-              page: this.context.pageName,
-              name: this.props.name,
-              value: e.target.textContent,
-            })
-        }}
-      >
-        {(adminOverride && adminOverride.value) ||
-          (value && value.node.value) ||
-          this.props.initial ||
-          'Lorem ipsum'}
-      </div>
+    value =
+      (adminOverride && adminOverride.value) ||
+      (value && value.node.value) ||
+      this.props.initial ||
+      'Lorem ipsum'
+
+    this.setState({ value })
+
+    if (
+      typeof window !== 'undefined' &&
+      window.parent &&
+      window.parent.editables
+    ) {
+      require.ensure(
+        ['draft-js', 'draft-js-import-html'],
+        () => {
+          const draftjs = require('draft-js')
+          const importHtml = require('draft-js-import-html')
+
+          this.Editor = draftjs.Editor
+          let contentState = importHtml.stateFromHTML(this.state.value)
+          this.setState({
+            editorState: draftjs.EditorState.createWithContent(contentState),
+          })
+        },
+        'draft-js-chunk' // chunk name - need to be set to something
+      )
+    }
+  }
+
+  // componentDidMount() {
+  //   if (
+  //     typeof window !== 'undefined' &&
+  //     window.parent &&
+  //     window.parent.editables
+  //   ) {
+  //     // import { Editor, EditorState } from 'draft-js'
+  //     // import { stateFromHTML } from 'draft-js-import-html'
+  //     Promise.all([import('draft-js'), import('draft-js-import-html')]).then(
+  //       ([draftjs, draftjsImportHtml]) => {
+  //         console.log(Editor)
+  //       }
+  //     )
+  //   }
+  //   // let contentState = stateFromHTML('<p>hello world</p>')
+  //   // this.state = { editorState: EditorState.createWithContent(contentState) }
+  //   // this.onChange = editorState => this.setState({ editorState })
+  // }
+
+  render() {
+    return this.state.editorState ? (
+      <this.Editor
+        editorState={this.state.editorState}
+        onChange={this.onChange}
+      />
+    ) : (
+      <div>{this.state.value}</div>
     )
+
+    // return (
+    //   <div
+    //     contentEditable
+    //     onBlur={e => {
+    //       window.parent &&
+    //         window.parent.save({
+    //           page: this.context.pageName,
+    //           name: this.props.name,
+    //           value: e.target.textContent,
+    //         })
+    //     }}
+    //   >
+    //     {this.state.value}
+    //   </div>
+    // )
   }
 }
 
